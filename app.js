2f398d8107f7b7afe0da1b6a3275390c41c3542e
Austin Badura <austinbadur@gmail.com>
2023-01-24 17:10:51 -0600

    EJS new stuff

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const app = express();
 
+var items = [];
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
+    var today = new Date();
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var currentDay = today.toLocaleString("en-US", options);
+
+    res.render("list", {kindOfDay: currentDay, newListItems: items});
+});
+
+app.post("/", function(req, res){
+    items.push(req.body.newItem);
     
+    res.redirect("/")
 });
 
 app.listen(3000, function(){
