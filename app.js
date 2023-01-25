df69abe44556c9f98312d4f708437b3261a664ac
Austin Badura <austinbadur@gmail.com>
2023-01-24 18:06:09 -0600

    EJS new stuff v2

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
     var today = new Date();
